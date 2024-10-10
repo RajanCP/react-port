0930c506269823f69bba58d56018f68e81c44f23
Rajan <rjnptl2000@gmail.com>
2024-10-11 00:59:01 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+   build: {
+    assetsDir: 'assets',  // Specify the folder for assets (default is 'assets')
+  },
   base:"/final-port/"
 })
